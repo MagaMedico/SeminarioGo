c05acca445da125a6d20f715786afe54927bcf8d
Cecilia Carlon <ceciliacarlon2@gmail.com>
2021-09-23 16:42:24 -0300

    Eliminados los comentarios

--- main_test.go
+++ main_test.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	//"os"
 	"testing"
 
-	//"github.com/hoisie/mustache"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -12,36 +10,6 @@ func TestDivideString(t *testing.T) {
 	s := divideString("")
 	assert.Equal(t, s, "", "El string esta vacio")
 }
-/*
-func TestAsign(t *testing.T) {
-	var cases = []struct {
-		Type   string
-		Length int
-		Value  string
-	}{
-		{"NNN", 987654321, "10"},
-		{"", 0, ""},
-		{"", 10, "JDD"},
-		{"0004", 5000000, ""},
-	}
-	for _, testData := range cases {
-		lt1 := len(testData.Type)
-		if lt1 != 2 {
-			m := (lt1 != 2)
-			assert.Equal(t, m, "El tamaño de el primer parametro debe ser 2")
-		}
-		lt2 := testData.Length
-		if lt2 > 99 {
-			m := lt2 > 99
-			assert.Equal(t, m, "El segundo parametro debe ser menor a 99")
-		}
-		lt3 := len(testData.Value)
-		if lt3 == 0 {
-			m := (lt3 == 0)
-			assert.Equal(t, m, "El tercer parametro esta vacio")
-		}
-	}
-}*/
 
 func TestAsign(t *testing.T) {
 	var cases = []struct {
@@ -59,99 +27,11 @@ func TestAsign(t *testing.T) {
 	for _, testData := range cases {
 
 		a := divideString(testData.Input)
-		//a := divideString("TX02AB")
 
-		/*var a []string
-
-		a[0] = testData.Type
-		a[1] = testData.Length
-		a[2] = testData.Value*/
-		
-		test := asign(a)	//Convert to string
+		test := asign(a) //Convert to string
 
 		assert.Equal(t, testData.Type, test.Type)
 		assert.Equal(t, testData.Value, test.Value)
 		assert.Equal(t, testData.Length, test.Length)
 	}
 }
-
-/*Funcion nueva
-func TestAsign(t *testing.T) {
-	
-	case1 := []string{"NNN", "987654321", "10"}
-	asign(case1)
-	case2 := []string{"", "0", ""}
-	case3 := []string{"", "10", "JDD"}
-	case4 := []string{"0004", "5000000", ""}
-
-	c := 0
-
-	for _, testData := range case1 {
-		lt1 := len(testData)
-		if c == 0 && lt1 != 2 {
-			m := (lt1 != 2)
-			assert.Equal(t, m, "El tamaño de el primer parametro debe ser 2")
-		}
-		lt2 := len(testData)
-		if c == 1 && lt2 != 2 {
-			m := lt2 != 2
-			assert.Equal(t, m, "El segundo parametro debe ser menor a 99")
-		}
-		lt3 := len(testData)
-		if c == 2 && lt3 == 0 {
-			m := (lt3 == 0)
-			assert.Equal(t, m, "El tercer parametro esta vacio")
-		}
-		c++
-	}
-}*/
-
-/*
-func TestParser(t *testing.T) {
-	var cases = []struct {
-		Input   string // input string in order to be parsed
-		Success bool   // paser result
-		Type    string // the input type
-		Value   string // the input value
-		Length  int    // value length
-	}{
-		{"TX02AB", true, "TX", "AB", 2},
-		{"NN100987654321", true, "NN", "0987654321", 10},
-		{"TX06ABCDE", false, "", "", 0},
-		{"NN04000A", false, "", "", 0},
-	}
-
-	for _, testData := range cases {
-		r, err := mustache.ParseString([]byte(testData.Input))
-		// acá agregar chequeos propios del test por ejemplo:
-		assert.Equal(t, err == nil, testData.Success)
-	}
-}*/
-
-/*func TestMain(m *testing.M) {
-	os.Exit(m.Run());
-}*/
-
-/*func TestMain(t *testing.T) {
-	var cases = []struct {
-		Input   string // input string in order to be parsed
-		Success bool   // paser result
-		Type    string // the input type
-		Value   string // the input value
-		Length  int    // value length
-	}{
-		{"TX02AB", true, "TX", "AB", 2},
-		{"NN100987654321", true, "NN", "0987654321", 10},
-		{"TX06ABCDE", false, "", "", 0},
-		{"NN04000A", false, "", "", 0},
-	}
-	for _, testData := range cases {
-
-		s := divideString(testData.Input)
-		result := asign(s)	//Convert to string
-
-		assert.Equal(t, testData.Type, result.Type)
-		assert.Equal(t, testData.Value, result.Value)
-		assert.Equal(t, testData.Length, result.Length)
-	}
-}*/
